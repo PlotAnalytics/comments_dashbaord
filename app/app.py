5513843c71f3f6eb349ea775bff679013b3e234c
housebarnavyyard <engineering@housebar.com>
2024-11-04 22:33:54 +0530

    add changes

--- app/app.py
+++ app/app.py
@@ -90,5 +90,7 @@ def index():
 
     return render_template("index.html", chart1=chart1, chart2=chart2, chart3=chart3)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = 8080
+    app.run(host="0.0.0.0", port=port)
+
